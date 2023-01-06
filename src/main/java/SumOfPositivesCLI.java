5f5f8a75d3d3cd79a4b639c06276c5de57fb904f
daytien <daytienw@gmail.com>
2023-01-05 22:06:44 -0600

    initialization

--- src/main/java/SumOfPositivesCLI.java
+++ src/main/java/SumOfPositivesCLI.java
@@ -1,5 +1,12 @@
 public class SumOfPositivesCLI {
     public static void main(String[] args) {
-
+        int sum = 0;
+        for (String arg : args) {
+            int num = Integer.parseInt(arg);
+            if (num > 0) {
+                sum += num;
+            }
+        }
+        System.out.print("# => prints " + sum);
     }
 }
